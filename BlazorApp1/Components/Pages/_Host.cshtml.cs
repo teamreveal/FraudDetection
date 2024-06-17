5741df6cc71f070c5c877bfb016650a27d4f427a
Benny John <benny.john@hrblock.com>
2024-06-17 13:00:54 +0530

    Create

---
+++ BlazorApp1/Components/Pages/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp1.Components.Pages
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BlazorApp1/wwwroot/mouseInteraction.js
@@ -0,0 +1,35 @@
+﻿window.checkMouseInteraction = function () {
+    var hasCtrlKey = false;
+    var hasTabKey = false;
+
+    document.addEventListener('keydown', function (event) {
+        if (event.ctrlKey && event.code === 'KeyT') {
+            console.log("alert");
+            hasCtrlKey = true;
+            hasTabKey = true;
+        }
+    });
+
+    window.addEventListener('blur', function () {
+        if (hasCtrlKey && hasTabKey) {
+            DotNet.invokeMethodAsync('BlazorApp1', 'HandleMouseInteraction', 'newTab');
+
+            DotNet.invokeMethodAsync('BlazorApp1', 'HandleMouseInteraction', 'newTab');
+            hasCtrlKey = false;
+            hasTabKey = false;
+        }
+    });
+
+    window.addEventListener('focus', function () {
+        hasCtrlKey = false;
+        hasTabKey = false;
+    });
+
+    return !hasCtrlKey;
+};
+window.blazorMouseInterop = {
+    handleMouseClick: function (elementId) {
+        var element = document.getElementById(elementId);
+        element.click();
+    }
+};
